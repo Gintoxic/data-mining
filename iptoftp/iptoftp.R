5b8993b06e140dc4eb57e2e1dc46d4c69b93e53e
Gintoxic <gintoxic@gmail.com>
2013-10-27 20:16:56 +0100

    changes iptoftp

--- iptoftp/iptoftp.R
+++ iptoftp/iptoftp.R
@@ -1,22 +1,19 @@
 ## R-Script: IP Ermitteln, TXT-Datei erstellen, TXT per FTP hochladen
 
-source("../../credentials.R")
-
 library(RCurl)
 
 # IP Ermitteln
 ipurl<-"http://api.externalip.net/ip"
-
 ip<-readLines(ipurl)
 
 # TXT-Datei erstellen
 sysTime<-Sys.time()
 setwd("D:/Work/data-mining/iptoftp")
+source("../../credentials.R")
+
 
 dataFolder<-"../../data/iptoftp/"
-
 fileName<-paste(dataFolder,format(sysTime, "%y%m%d_%H%M%S"),".txt", sep="")
-
 lastUpdate<-paste("Last Update:",format(sysTime, "%y-%m-%d %H:%M:%S"))
 
 fileConn<-file(fileName)
