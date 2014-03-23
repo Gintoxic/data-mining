c577064b082aaf95f34533fba8b9db88ce4ea787
Gintoxic <gintoxic@gmail.com>
2014-03-23 22:40:28 +0100

    init ddemo

---
+++ sql/auszaehlung_flightaware.sql
@@ -0,0 +1,19 @@
+﻿
+with orig as
+(
+select origin_icao as icao, count(*) as num_orig from fa_2013_char
+group by origin_icao
+), 
+dest as
+(select destination_icao as icao, count(*) as num_dest from fa_2013_char
+group by destination_icao
+), 
+numbers as
+(
+select o.icao, num_orig, num_dest from orig o inner join dest d
+on o.icao=d.icao
+
+)
+select n.*, a.city, a.country from numbers n left join airports a
+on n.icao=a.icao
+order by num_orig+num_dest desc
---
+++ sql/erster_abgleich.sql
@@ -0,0 +1,18 @@
+﻿with al_prep as
+(
+select * from airlines
+where iata is not null and iata not in ('-', '')
+and icao not in ('', '\N', '***', '???', '===')
+), 
+ba_prep as
+(
+select bt.*, ap.icao,ap.icao||bt.flugnummer as ident, abreisedatum::date as dep_date from c_bayer_test1 bt left join al_prep ap
+on bt.code_transportunternehmen=ap.iata
+)
+--select * from ba_prep
+,
+fa_prep as
+(select f.*,substr(scheduled_departure_time,1,10)::date as dep_date from fa_2013_char f)
+select * from ba_prep bp inner join fa_prep fp
+on bp.ident=fp.ident and bp.dep_date=fp.dep_date
+order by reisedauer, cancelled
