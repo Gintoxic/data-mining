310c87a8c69a98ace942e929d6dc3536b67f93c6
Gintoxic <gintoxic@gmail.com>
2014-04-13 16:45:21 +0200

    remove old queries

---
+++ database/create/create_v_flights.sql
@@ -0,0 +1,43 @@
+﻿create view v_flights as
+with flights_temp1 as
+(
+select substr(ident,1,3) as airline_icao,
+substr(ident, 4, length(ident)-3) as flightnumber,
+
+origin_icao, 
+origin_name, 
+origin_city,
+
+dest_icao, 
+dest_name, 
+dest_city,
+
+to_timestamp(dep_sched_local, 'YYYY-MM-DD HH24:MI'):: timestamp without time zone as dep_sched_local,
+substr(dep_sched_local, 18,4) as dep_tz,
+to_timestamp(dep_sched_utc, 'YYYY-MM-DD HH24:MI') :: timestamp without time zone as dep_sched_utc,
+
+to_timestamp(arr_sched_local, 'YYYY-MM-DD HH24:MI'):: timestamp without time zone as arr_sched_local,
+substr(arr_sched_local, 18,4) as arr_tz,
+to_timestamp(dep_sched_utc, 'YYYY-MM-DD HH24:MI') :: timestamp without time zone as arr_sched_utc,
+
+to_timestamp(dep_act_local, 'YYYY-MM-DD HH24:MI'):: timestamp without time zone as dep_act_local,
+to_timestamp(dep_act_utc, 'YYYY-MM-DD HH24:MI'):: timestamp without time zone as dep_act_utc,
+
+to_timestamp(arr_act_local, 'YYYY-MM-DD HH24:MI'):: timestamp without time zone as arr_act_local,
+to_timestamp(arr_act_utc, 'YYYY-MM-DD HH24:MI'):: timestamp without time zone as arr_act_utc,
+
+registration,
+code_shares,
+distance, 
+enroute, 
+cancelled
+
+from staging.flights_2013 
+)
+select ft.*, va1.iata as origin_iata,
+va2.iata as dest_iata
+ from flights_temp1 ft 
+left join v_airports_iaic va1
+on ft.origin_icao=va1.icao
+left join v_airports_iaic va2
+on ft.dest_icao=va2.icao
--- database/create_airlines_iaic.sql
+++
@@ -1,6 +0,0 @@
-﻿create view airlines_iaic as
-select a.*, 
-row_number() over (partition by iata) as num_iata, 
-row_number() over (partition by icao) as num_icao
- from airlines a
- where iata  !='' and icao not in ('\N', '', ':::', '\\''\\', '???', '---')
