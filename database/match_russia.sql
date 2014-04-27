c21abf2d9f968c21032fb633d926b8d3f5ce9fb3
Gintoxic <gintoxic@gmail.com>
2014-04-27 23:02:23 +0200

    rematch russia full

---
+++ database/match_russia.sql
@@ -0,0 +1,29 @@
+﻿with distinct_iata as
+( 
+	select distinct on (iata) iata, icao, name,country from staging.of_airlines a
+	where iata  not in ('', 'N/A') and  icao not in ('', 'N/A')
+), 
+russia_adv as
+(
+select r.*, di.icao as airline_icao
+
+--to_date(r.dep_date, 'DD.MM.YYYY') as de_date_test
+
+from staging.cd_russia_test r left join distinct_iata di
+on r.carrier_code=di.iata
+where flightnumber!='ST1979'
+)
+--select * from russia_adv
+
+
+select r.*, f.* 
+from russia_adv r
+inner join staging.v_flights f
+on 
+to_date(r.dep_date, 'DD.MM.YYYY')=date_trunc('day',f.dep_sched_local)
+ and r.flightnumber::numeric = f.flightnumber::numeric
+and r.carrier_code=f.airline_iata
+and r.dest_iata=f.dest_iata
+and r.origin_iata=f.origin_iata
+--order by f.flight_id nulls last, r.cd_id
+order by dep_act_local
