f8154000ec07f1f82ddb00625f1e640ad17f4ac5
Gintoxic <gintoxic@gmail.com>
2014-08-04 23:01:27 +0200

    customers 1,2,3

---
+++ database/checks/check_timezones.sql
@@ -0,0 +1,10 @@
+﻿with timezones as
+(
+select distinct dep_tz as tz, timezone_dif_dep as timezone_dif from v_flights
+union
+select distinct arr_tz as tz, timezone_dif_arr as timezone_dif from v_flights
+)
+select * from timezones
+order by timezone_dif desc, tz
+
+--select * from v_flights
---
+++ database/matches/match_001_001.sql
@@ -0,0 +1,29 @@
+﻿--select count(*) from cd_001_01 
+
+with 
+distinct_flights as
+(
+	select 
+	distinct on (airline_iata, flightnumber, date_trunc('day', dep_sched_local), origin_iata, dest_iata)
+	airline_iata, flightnumber, date_trunc('day', dep_sched_local) as dep_sched_local_date, origin_iata, dest_iata,
+	flight_id, cancelled, diftime_utc, type_dist from v_flights
+),
+
+valid_rows as
+(
+	select c.* from cd_001_01 c
+	where flightnumber  !~ '[^0-9]'
+),
+
+match as
+(
+	select vr.*, f.flight_id, f.cancelled, f.diftime_utc, f.type_dist
+	from valid_rows vr inner join distinct_flights f
+	on vr.airline_iata=f.airline_iata
+	and vr.flightnumber::numeric=f.flightnumber::numeric
+	and vr.dep_sched_local_date=f.dep_sched_local_date
+	and vr.origin_iata=f.origin_iata
+	and vr.dest_iata=f.dest_iata
+)
+select cancelled, count(*), count(distinct flight_id), count(distinct import_counter) from match
+group by cancelled
